b2ccb47e373ab7cfb3f4e3eb1bdcaf169532269f
Dmitrii Cetvericov <d.cetvericov@live.de>
2021-12-30 00:03:48 +0100

    Implement SmartLabSolver + tests :-)

---
+++ LabSolver.Tests/SmartLabSolverTests.cs
@@ -0,0 +1,68 @@
+﻿using LabSolver.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LabSolver.Tests
+{
+    public class SmartLabSolverTests
+    {
+        private readonly string exampleLabyrinth =
+            "3 4 5\r\n" +
+            "S....\r\n" +
+            ".###.\r\n" +
+            ".##..\r\n" +
+            "###.#\r\n" +
+            "\r\n" +
+            "#####\r\n" +
+            "#####\r\n" +
+            "##.##\r\n" +
+            "##...\r\n" +
+            "\r\n" +
+            "#####\r\n" +
+            "#####\r\n" +
+            "#.###\r\n" +
+            "####E\r\n" +
+            "\r\n" +
+            "1 3 3\r\n" +
+            "S##\r\n" +
+            "#E#\r\n" +
+            "###\r\n" +
+            "\r\n" +
+            "0 0 0";
+
+        private ILabParser parser;
+
+        public SmartLabSolverTests()
+        {
+            parser = new StringLabParser(exampleLabyrinth);
+        }
+
+        [Fact]
+        public async Task SolverMustSolveFirstExample()
+        {
+            var firstLab = (await parser.Parse().ConfigureAwait(false)).Result.First();
+
+            var solver = new SmartLabSolver();
+            var solveResult = await solver.SolveLabyrinth(firstLab).ConfigureAwait(false);
+
+            Assert.True(solveResult.Success);
+
+            Assert.Equal(11u, solveResult.Result);
+        }
+
+        [Fact]
+        public async Task SolverMustSolveSecondExample()
+        {
+            var firstLab = (await parser.Parse().ConfigureAwait(false)).Result.ElementAt(1);
+
+            var solver = new SmartLabSolver();
+            var solveResult = await solver.SolveLabyrinth(firstLab).ConfigureAwait(false);
+
+            Assert.False(solveResult.Success);
+        }
+    }
+}
---
+++ LabSolver/SmartLabSolver.cs
@@ -0,0 +1,67 @@
+﻿using LabSolver.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSolver
+{
+    public class SmartLabSolver : ILabSolver
+    {
+        public async Task<LabResult<uint>> SolveLabyrinth(ILabStartNode startNode)
+        {
+            return await SolveSmart(default, startNode).ConfigureAwait(false);
+        }
+
+        private async Task<LabResult<uint>> SolveSmart(ILabNode lastNode, ILabNode nextNode)
+        {
+            // Remove lastNode from neighbours so that we don't go the same way multiple times.
+            if (lastNode != null)
+            {
+                nextNode.Neighbours.Remove(lastNode);
+            }
+
+            // If the given node is the end node, we've solved the lab.
+            if (nextNode is ILabEndNode)
+            {
+                return new LabResult<uint>
+                {
+                    Success = true,
+                    Result = 0,
+                };
+            }
+
+            // Otherwise, we must determine if we have non-stone neighbours.
+            var maybeNonStoneNeighbours = nextNode.Neighbours.Where(node => node is ILabAirNode || node is ILabEndNode);
+
+            // If there are no non-stone neighbours, we can't escape this way.
+            if (!maybeNonStoneNeighbours.Any())
+            {
+                return new LabResult<uint>
+                {
+                    Success = false,
+                    Result = 0,
+                };
+            }
+
+            // Save the results.
+            var results = new List<LabResult<uint>>();
+
+            // Now, we must recursively continue our search.
+            foreach (var neighbour in maybeNonStoneNeighbours)
+            {
+                results.Add((await SolveSmart(nextNode, neighbour).ConfigureAwait(false)));
+            }
+
+            var firstResult = results.FirstOrDefault(results => results.Success);
+            if (firstResult != default)
+            {
+                firstResult.Result++;
+                return firstResult;
+            }
+
+            return results.FirstOrDefault();
+        }
+    }
+}
