d54dba72dde3f9bcac6c7dd3e06e4d6a3708c8d1
Dmitrii Cetvericov <d.cetvericov@live.de>
2021-12-30 15:46:34 +0100

    Add console app.

---
+++ LabSolver.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using LabSolver;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Usage: LabSolver.ConsoleApp '<Path_To_File_With_Lab_Text>'");
+    return;
+}
+
+if (!File.Exists(args[0]))
+{
+    Console.WriteLine($"ERROR: File {args[0]} does not exist.");
+    return;
+}
+
+var parser = new StringLabParser(await File.ReadAllTextAsync(args[0]));
+
+var labs = await parser.Parse().ConfigureAwait(false);
+
+if (!labs.Success)
+{
+    Console.WriteLine($"ERROR: Unable to parse labyrinths: {labs.Message}.");
+    return;
+}
+
+var solver = new SmartLabSolver();
+
+foreach (var lab in labs.Result)
+{
+    var solveResult = await solver.SolveLabyrinth(lab);
+
+    if (solveResult.Success)
+    {
+        Console.WriteLine($"Entkommen in {solveResult.Result} Minute(n)!");
+    }
+    else
+    {
+        Console.WriteLine("Gefangen :-(");
+    }
+}
