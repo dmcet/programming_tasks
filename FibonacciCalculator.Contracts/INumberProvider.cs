8a74fcc430d2a9cf1dad9b937b6380f75d95f54a
Dmitrii Cetvericov <d.cetvericov@live.de>
2021-12-30 17:28:17 +0100

    Refactor everything a bit, implement first version of parser.

---
+++ FibonacciCalculator.Contracts/INumberProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciCalculator.Contracts
+{
+    /// <summary>
+    /// Parses CRLF-separated numbers from any kind of source.
+    /// </summary>
+    public interface INumberProvider
+    {
+        /// <summary>
+        /// Triggers the parsing.
+        /// </summary>
+        /// <returns>the parsed numbers.</returns>
+        /// <exception cref="ArgumentException">if the string is malformed.</exception>
+        Task<IEnumerable<uint>> ParseNumbers();
+    }
+}
---
+++ FibonacciCalculator.Tests/StringNumberProviderTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FibonacciCalculator.Tests
+{
+    public class StringNumberProviderTests
+    {
+        private Random random = new Random();
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(99)]
+        public async Task NumberProviderMustParseNumbers(int count)
+        {
+            var numberString = GenerateNumberString(count);
+
+            var numProvider = new StringNumberProvider(numberString);
+
+            var numbers = await numProvider.ParseNumbers().ConfigureAwait(false);
+
+            // Must be same amount of numbers.
+            Assert.Equal(count, numbers.Count());
+
+            // Test first entry.
+            Assert.Equal(numberString.Split("\r\n")[0], numbers.First().ToString());
+        }
+
+        [Fact]
+        public async Task NumberProviderMustThrowIfInvalidString()
+        {
+
+        }
+
+        /// <summary>
+        /// Generates a valid string with numbers.
+        /// </summary>
+        /// <param name="amount">the amount of numbers this string should contain.</param>
+        /// <returns></returns>
+        private string GenerateNumberString(int amount)
+        {
+            var stringBuilder = new StringBuilder();
+            for (var index = 0; index < amount; index++)
+            {
+                stringBuilder.Append(random.Next(0, 5001)).Append("\r\n");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
